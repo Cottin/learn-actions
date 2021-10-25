2b6beb011fbc4c2ef641bf60c6bc8ba0b43044fd
Victor Cottin <victor@cottin.se>
2021-10-25 18:22:46 +0200

    initial commit

---
+++ src/problems/easy/sum1toN.js
@@ -0,0 +1,16 @@
+
+// Write a program that takes an argument n and returns the sum of the numbers 1 to n (inclusive)
+const sum1toN = (n) => {
+
+	// Your code here...
+	return 6
+}
+
+const tests = [
+	[sum1toN, [3], 6],
+	[sum1toN, [4], 10],
+]
+
+export default tests
+
+
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const {merge} = require('webpack-merge');
+
+const isAnalyze = typeof process.env.BUNDLE_ANALYZE !== "undefined"
+
+const common = {
+	entry: './src/index.js',
+	plugins: [
+		new HtmlWebpackPlugin({
+			hash: true,
+			template: './src/index.html',
+		}),
+		isAnalyze && new BundleAnalyzerPlugin()
+	].filter(Boolean),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {}
+				}
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		]
+	}
+}
+
+const production = {
+	devtool: 'source-map',
+}
+
+const development = {
+	devtool: 'eval-source-map',
+}
+
+
+module.exports = (env, args) => {
+  switch(args.mode) {
+    case 'development':
+      return merge(common, development);
+    case 'production':
+      return merge(common, production);
+    default:
+      throw new Error('No matching configuration was found!');
+  }
+}
