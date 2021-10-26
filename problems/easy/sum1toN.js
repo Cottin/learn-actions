253e68e50b48f475f23c8c507b07ebf32fd238b9
Victor Cottin <victor@cottin.se>
2021-10-26 19:25:52 +0200

    resolved problem

--- problems/easy/sum1toN.js
+++ problems/easy/sum1toN.js
@@ -1,9 +1,8 @@
+import * as R from 'ramda'
 
 // Write a program that takes an argument n and returns the sum of the numbers 1 to n (inclusive)
 const sum1toN = (n) => {
-
-	// Your code here...
-	return 6
+	return R.pipe(R.range(1), R.sum)(n+1)
 }
 
 const tests = [
